29b74792e96cc800ebf9c3665b461b17da6c5e23
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-10-09 23:38:33 +0300

    Create main.cpp

---
+++ code/main.cpp
@@ -0,0 +1,14 @@
+// a_adv2222.cpp : This file contains the 'main' function. Program execution begins and ends there.
+using namespace std;
+#include <iostream>
+#include "enitiy.cpp"
+#include "monster.cpp"
+#include "player.cpp"
+#include "Normalplayer.cpp"
+#include "MonsterStealth.cpp"
+#include "MonsterFast.cpp"
+#include "a_adv2222.h"
+int main()
+{
+	
+}
