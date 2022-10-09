5f6d36c4d335de37f5ff47760d7df09737d8a8f8
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-10-09 20:12:50 +0300

    Update class enitiy.h

--- class enitiy.h
+++ class enitiy.h
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class enitiy{
-int x,y;
+class enitiy {
+private:
+	int x, y;
 
 public:
-// move from location to another location
-void move();
+	// move from location to another location
+	void move() {
+		int newX, newY;
+		cin >> newX >> newY;
+		this-> x = newX; this-> y = newY;
+	}
 
-// the render for the player and monster is different
-virtual void render()=0;
+	// the render for the player and monster is different
+	virtual void render() = 0;
 
-// the noise for the player and monster is different
-void mackeNoise();
+	// the noise for the player and monster is different
+	void mackeNoise() {
+		// the noise
+	}
 };
