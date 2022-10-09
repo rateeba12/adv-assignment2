9532d4efa6d8010c387d7c14677bc8e23c7f8f96
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-10-09 21:19:42 +0300

    Update and rename player.h to player.cpp

---
+++ player.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include "enitiy.cpp" 
+class player{
+    
+    private:
+    enitiy * Enitiy;
+    public:
+   
+   void mackeNoise(){
+      //nothing
+   }
+
+}
