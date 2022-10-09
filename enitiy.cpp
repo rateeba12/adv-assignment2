e222c8e213c072868d0f98f86af6e94cd6d2a09a
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-10-09 21:18:38 +0300

    Update and rename class enitiy.cpp to enitiy.cpp

---
+++ enitiy.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+class enitiy {
+
+public:
+	int x; int y;
+
+	// move from location to another location 
+	virtual void move() {
+		int newX, newY;
+		cin >> newX >> newY;
+		this-> x = newX; this-> y = newY;
+	}
+
+	// the render for the player and monster is different
+	virtual void render(){
+	    string appearance ="visible";
+	}
+
+	// the noise for the player and monster is different
+	virtual void mackeNoise()=0;
+};
